0f1755243db5be2720679d302b9aac559c8f02df
thrsvv <theresamarilao1177@gmail.com>
2025-11-25 23:18:08 +0800

    feat: Implement Products & Categories API and fix migration order

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 // Category Routes
 Route::apiResource('categories', CategoryController::class);
